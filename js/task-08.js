01f8fa20ea960e889ec6ef3fa67e505ebf547240
Zaslavskyi <bbaly89@gmail.com>
2023-03-01 20:19:33 +0200

    fixed..

--- js/task-08.js
+++ js/task-08.js
@@ -7,16 +7,14 @@ form.addEventListener('submit', (event) => {
         elements: { email, password },
     } = event.currentTarget;
 
-
-
     if (email.value === "" || password.value === "") {
     return alert("Усі поля повинні бути заповнені!");
     };
     
-        const inputValue = {
+    const inputValue = {
         email: email.value,
         password: password.value
-        }
+    }
     console.log(inputValue);
 
     event.target.reset();
